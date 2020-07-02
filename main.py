10f45a8935f2d3c35d34deca3f349aacd931587d
ashmeetsmonga <ashmeetsmonga@gmail.com>
2020-07-02 17:00:43 +0530

    Displaying the unsolved board

--- main.py
+++ main.py
@@ -1,14 +1,91 @@
 import pygame
+pygame.font.init()
+
+class Grid:
+    board =[
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+    def __init__(self, rows, cols, width, height, win):
+        self.rows = rows
+        self.cols = cols
+        self.width = width
+        self.height = height
+        self.model = None
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        #self.update_model()
+        self.selected = None
+        self.win = win
+
+    def draw(self):
+        # Draw grid lines
+        gap = self.width / 9
+        for i in range(self.rows + 1):
+            if i % 3 == 0 and i != 0:
+                thick = 4
+            else:
+                thick = 1
+            pygame.draw.line(self.win, (0, 0, 0), (0, i * gap), (self.width, i * gap), thick)
+            pygame.draw.line(self.win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
+
+        # Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(self.win)
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__(self, value, row, col, width, height):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+
+    def draw(self, win):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        if not (self.value == 0):
+            text = fnt.render(str(self.value), True, (0, 0, 0))
+            win.blit(text,(x +  (gap / 2 - text.get_width() / 2), y + gap / 2 - text.get_height() / 2))
+
+
+
+def redraw(win, board):
+    win.fill((255, 255, 255))
+
+    #Draw Board
+    board.draw()
+
 
 def main():
-    win = pygame.display.set_mode((540, 600))
+    win = pygame.display.set_mode((540, 540))
     pygame.display.set_caption("Sudoku Solver")
+    board = Grid(9, 9, 540, 540, win)
     run = True
     while run:
         for event in pygame.event.get():
-            win.fill((255, 255, 255))
             if event.type == pygame.QUIT:
                 run = False
-            pygame.display.update()
+            #if event.type == pygame.K_SPACE:
+                #board.solve_gui()
+
+        redraw(win, board)
+        pygame.display.update()
 
 main()
