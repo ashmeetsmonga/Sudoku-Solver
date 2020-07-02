7adeecab257b6b72148c9ba0042a7e18c5f98c51
ashmeetsmonga <ashmeetsmonga@gmail.com>
2020-07-02 19:59:10 +0530

    Implemented Backtracking

--- main.py
+++ main.py
@@ -41,6 +41,33 @@ class Grid:
             for j in range(self.cols):
                 self.cubes[i][j].draw(self.win)
 
+    def solve_gui(self):
+        find = find_empty(self.board)
+        if not find:
+            return True
+        else:
+            row, col = find
+
+        for i in range(1, 10):
+            if valid(self.board, i, (row, col)):
+                self.board[row][col] = i
+                self.cubes[row][col].set(i)
+                self.cubes[row][col].draw_change(self.win, True)
+                pygame.display.update()
+                pygame.time.delay(100)
+
+                if self.solve_gui():
+                    return True
+
+                self.board[row][col] = 0
+                self.cubes[row][col].set(0)
+                self.cubes[row][col].draw_change(self.win, False)
+                pygame.display.update()
+                pygame.time.delay(100)
+
+        return False
+
+
 class Cube:
     rows = 9
     cols = 9
@@ -64,17 +91,69 @@ class Cube:
             text = fnt.render(str(self.value), True, (0, 0, 0))
             win.blit(text,(x +  (gap / 2 - text.get_width() / 2), y + gap / 2 - text.get_height() / 2))
 
+    def set(self, val):
+        self.value = val
+
+    def draw_change(self, win, g = True):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        pygame.draw.rect(win, (255, 255, 255), (x, y, gap, gap), 0)
+
+        text = fnt.render(str(self.value), True, (0, 0, 0))
+        win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + gap / 2 - text.get_height() / 2))
+
+        if g:
+            pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
+        else:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+
+def valid(board, num, pos):
+    # Checking the row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Checking the col
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Checking the 3 X 3 box
+    box_x = (pos[1] // 3) * 3
+    box_y = (pos[0] // 3) * 3
+
+    for i in range(box_y, box_y + 3):
+        for j in range(box_x, box_x + 3):
+            if board[i][j] == num and pos != (i, j):
+                return False
+
+    return True
 
 
 def redraw(win, board):
     win.fill((255, 255, 255))
-
+    fnt = pygame.font.SysFont("comicsans", 30)
+    start_label = fnt.render("Press Space Bar to start", True, (0, 0, 0))
+    win.blit(start_label, (270 - start_label.get_width() / 2, 560))
     #Draw Board
     board.draw()
 
 
 def main():
-    win = pygame.display.set_mode((540, 540))
+    win = pygame.display.set_mode((540, 600))
     pygame.display.set_caption("Sudoku Solver")
     board = Grid(9, 9, 540, 540, win)
     run = True
@@ -82,8 +161,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            #if event.type == pygame.K_SPACE:
-                #board.solve_gui()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    board.solve_gui()
 
         redraw(win, board)
         pygame.display.update()
